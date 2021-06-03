55e995c8e724a283d5dd9be6dad9c205bffa4376
F3yl1x <felix.kohlenberger@gmail.com>
2021-06-03 10:55:10 +0200

    fertig

--- Probeklausur.cpp
+++ Probeklausur.cpp
@@ -42,33 +42,106 @@ std::vector<int> operator+(const std::vector<int> &eingabe1, const std::vector<i
     return dieser;
 }
 
-int main(){
-    func("Testen");
-    std::cout << std::endl;
+int foo(int x, int y)
+{
+    std::cout << x;
+    return x + y;
+}
 
-    std::vector<int> test;
-    test.push_back(4);
-    test.push_back(2);
-    test.push_back(6);
-    std::cout<< produkt(test) << std::endl;
-    std::cout<<verdoppeln("Hallo")<<std::endl;
-    std::vector<int> vec1;
-    vec1.push_back(1);
-    vec1.push_back(2);
-
-    std::vector<int> vec2;
-    vec2.push_back(3);
-    vec2.push_back(4);
-
-    std::vector<int> vec3;
-
-    vec3= vec1 + vec2;
-
-    for(int i = 0; i<vec3.size(); i++)
+void bar(int &k)
+{
+    int y = k;
+    for(char b = 3; b >= -1; b--)
     {
-        std::cout<< vec3[i]<<std::endl;
+        k = foo(y,b);
     }
+}
 
+class name {
+protected :
+std::string vorname = "Max";
+std::string nachname = "Mustermann";
+public :
+void print() { std::cout << vorname << " " << nachname << "\n"; }
+virtual int length() { return vorname.size() + nachname.size() + 1; }
+};
+
+
+class firmenname : public name {
+public :
+firmenname() { nachname = "AG"; }
+void print() { std::cout << nachname << "\n"; }
+virtual int length() { return nachname.size(); }
+};
+
+template <typename T>
+
+std::vector<T> replace(std::vector<T> v,T x,T y)
+{
+    for(int i = 0; i < v.size(); i++)
+    {
+        if(v[i]== x) 
+        {
+            v[i] = y;
+        }
+    }
+    return v;
+}
+
+
+
+
+
+int main(){
+    // func("Testen");
+    // std::cout << std::endl;
+
+    // std::vector<int> test;
+    // test.push_back(4);
+    // test.push_back(2);
+    // test.push_back(6);
+    // std::cout << produkt(test) << std::endl;
+    // std::cout << verdoppeln("Hallo") << std::endl;
+    // std::vector<int> vec1;
+    // vec1.push_back(1);
+    // vec1.push_back(2);
+
+    // std::vector<int> vec2;
+    // vec2.push_back(3);
+    // vec2.push_back(4);
+
+    // std::vector<int> vec3;
+
+    // vec3 = vec1 + vec2;
+
+    // for (int i = 0; i < vec3.size(); i++)
+    // {
+    //     std::cout << vec3[i] << std::endl;
+    // }
+    // int x = 1;
+    // while (x != 0) bar(x);
+
+    // name n;
+    // firmenname f;
+    // name &rn = n;
+    // name * pf = &f;
+
+    // n.print();
+    // std::cout << n.length() << "\n";
+    // f.print();
+    // std::cout << f.length() << "\n";
+
+    // rn.print();
+    // std::cout << rn.length() << "\n";
+    // pf->print ();
+    // std::cout << pf ->length () << "\n";
+
+    std::vector<int> vec1 = {1,2,2,4,2,6};
+    std::vector<int> vec2 = replace(vec1,2,10);
+    for (int i = 0; i < vec2.size(); i++)
+    {
+        std::cout<< vec2[i]<< std::endl;
+    }
 
     return 0;
 }
