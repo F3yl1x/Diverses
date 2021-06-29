98918ed265a38d2452db651b8a8c04cbb3a0bca0
F3yl1x <felix.kohlenberger@gmail.com>
2021-06-29 09:49:37 +0200

    d

--- Python/Test.py
+++ Python/Test.py
@@ -1,13 +1,67 @@
-msg1 = "Hello"
-msg2 = "World"
+#------------BubbleSort------------------
+def bubblesort(arr):
+    if len(arr) == 1:
+        return arr
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-print(msg1,msg2)
+array = [64, 34, 25, 12, 22, 11, 90]
+print(array)
+bubblesort(array)
+print(array)
+
+#----------------Turtle----------------
+
+import turtle
+
+turtle.setup(1280,720)                # Fenstergröße
+wn = turtle.Screen()                 # wn repräsentiert das Fenster 
+wn.title("Tastendruck-Ereignisse")   # Fenster Titel
+wn.bgcolor("lightgray")              # Hintergrundfarbe
+schildkroete = turtle.Turtle()       # Schildkröte anlegen
+
+# Event-Handler:
+def h1():
+   schildkroete.forward(50)
+
+def h2():
+   schildkroete.left(15)
+
+def h3():
+   schildkroete.right(15)
+
+def h4():
+    wn.bye()                        
+
+def h5(x, y):
+   schildkroete.penup()
+   schildkroete.goto(x, y)
+   schildkroete.pendown()
+
+# Verknüpfung herstellen zwischen Tastenereignissen und Handlern
+wn.onkey(h1, "Up")
+wn.onkey(h2, "Left")
+wn.onkey(h3, "Right")
+wn.onkey(h4, "q")
+wn.onclick(h5)  # "onclick" reagiert auf Mausklick
 
 
-import matplotlib.pyplot as plt
-import numpy as np
+# Mit "listen" fängt die Anwendung an nach Ereignissen zu "horchen".
+# Ein richtiger Tastendruck löst den zugehörigen Eventhandler aus.
+wn.listen()
+wn.mainloop()
 
-x = np.linspace(-20, 20, 1000)  # Create a list of evenly-spaced numbers over the range
-func = np.sin(x)/x
-plt.plot(x, func)       # Plot the sine of each x point
-plt.show()
+
+
+
+# ---------MathLab-----------------
+#import matplotlib.pyplot as plt
+#import numpy as np
+
+#x = np.linspace(-20, 20, 1000)  # Create a list of evenly-spaced numbers over the range
+#func = np.sin(x)/x
+#plt.plot(x, func)       # Plot the sine of each x point
+#plt.show()
