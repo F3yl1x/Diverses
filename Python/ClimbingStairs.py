c94409c924f394d0a6da56e55ca37f5bf681ffcd
F3yl1x <felix.kohlenberger@gmail.com>
2021-06-29 16:06:59 +0200

    h

--- Python/ClimbingStairs.py
+++ Python/ClimbingStairs.py
@@ -1,10 +1,26 @@
 class ClimbingStairs:
-    
-    @staticmethod
+    moeglichkeiten = 0
+
     def climbing_stairs(number_of_stairs: int) -> int:
-        # Implement your solution here
-        pass
+        if(number_of_stairs <= 1):
+            return 1
+        elif(number_of_stairs == 2):
+            return 2
+        elif():
+            ClimbingStairs:merker(1,moeglichkeiten,number_of_stairs)
+            merker(2,moeglichkeiten,number_of_stairs)
+            return moeglichkeiten
+
+    def merker(number_s,moeglichkeiten,steps):
+        if(number_s +1 <= steps):
+            moeglichkeiten +1
+            return
+        elif(number_s + 2 == steps):
+            moeglichkeiten +1
+            return
+        elif():
+            merker(1,moeglichkeiten,steps)
+            merker(2,moeglichkeiten,steps)
 
 class1 = ClimbingStairs()
-print("Test")
-print(class1.climbing_stairs(10))
+print(class1.climbing_stairs(3))
